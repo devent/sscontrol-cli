b18d4e14a8af0514aa2f3cc59f047603f3fb50a6
Erwin Mueller <erwin.mueller@deventm.org>
2013-08-14 21:15:06 +0200

    Add mail postfix mysql service
    #2610

---
+++ sscontrol-cli-app/src/test/resources/ubuntu_10_04/MailMysql.groovy
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2013 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * This file is part of sscontrol-mail-postfix.
+ *
+ * sscontrol-mail-postfix is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * sscontrol-mail-postfix is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with sscontrol-mail-postfix. If not, see <http://www.gnu.org/licenses/>.
+ */
+package ubuntu_10_04
+
+mail {
+	bind_addresses all
+
+	relay "smtp.relayhost.com"
+	name "mail.example.com"
+	origin "example.com"
+	database "maildb" user "root" password "password"
+
+	masquerade {
+		domains "mail.example.com"
+		users "root"
+	}
+
+	domain "localhost.localdomain", { catchall destination: "@localhost" }
+
+	domain "localhost", {
+		alias "postmaster", destination: "root"
+		alias "sysadmin", destination: "root"
+		alias "webmaster", destination: "root"
+		alias "abuse", destination: "root"
+		alias "root", destination: "root"
+		catchall destination: "root"
+		user "root", password: "rootpasswd"
+	}
+
+	domain "ubuntutest.com", { user "devent", password: "deventpasswd" }
+}
